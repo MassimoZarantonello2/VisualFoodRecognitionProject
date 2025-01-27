ea5f1867238650b9658bf3b8e9ae03a5282a14c8
AnnaMarika01 <a.biasco@campus.unimib.it>
2025-01-28 00:02:01 +0100

    Add image enhancement and noise detection pipelines
    Introduce new modules to address degraded image processing with noise detection, enhancement, and metrics analysis. Updates include robust deblurring, adaptive filtering, and noise reduction techniques.

---
+++ pipeline_degraded/some_images.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+from image_enhancement import *
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from PIL import Image
+import json
+
+from scripts.ImageDataset import ImageDataset
+
+load_dotenv()
+
+test_path = '../ground_truth/new_val_info.csv'
+test_image_path = os.getenv('TEST_IMAGE_PATH')
+# test_image_path = os.getenv('TRAIN_SET')
+
+test_table = pd.read_csv(test_path, header=None, names=['image_id', 'label'])
+
+test_dataset = ImageDataset(test_table, test_image_path, train=False)
+
+for i in range(len(test_dataset)):
+    image=test_dataset.get_image_by_index(i)
+    blurry_metrics = detect_blurriness(image)
+
+    if blurry_metrics["laplacian_variance"]  < 20:
+        image.show()
